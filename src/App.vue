afd8ca44812856e3ac6002c17c20d4bfab593653
tdedetd <tdedetd@gmail.com>
2022-07-15 04:04:23 +0300

    package.json: leaflet; map of spb

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,26 @@
 <script setup lang="ts">
+  import L from 'leaflet';
 
+  setTimeout(() => {
+    const map = L.map('map', {
+      center: [59.95, 30.31667],
+      zoom: 10
+    });
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '© OpenStreetMap'
+    }).addTo(map);
+  });
 </script>
 
 <template>
-  <div>123</div>
+  <div id="map"></div>
 </template>
 
 <style scoped>
-
+  #map {
+    width: 640px;
+    height: 480px;
+  }
 </style>
